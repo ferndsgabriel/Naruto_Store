acc47d50b1077c52df801f799bd0e07f5b76af48
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2023-10-04 21:39:21 -0300

    first commit

---
+++ src/pages/components/header/index.tsx
@@ -0,0 +1,19 @@
+import {AiOutlineSearch,AiOutlineShoppingCart,AiOutlineMenu} from "react-icons/ai";
+import styles from "./styles.module.scss";
+export function Header(){
+    return(
+        <header className={styles.container}>
+            <img src="vila.svg" alt="folha icone"
+            className={styles.iconVila}/>
+            <label className={styles.labelSearch}>
+                <input type="search" className={styles.inputSearch}
+                placeholder="Busca..."/>
+                <p><AiOutlineSearch/></p>
+            </label>
+            <div className={styles.buttonsHeader}>
+                <button><AiOutlineShoppingCart/></button>
+                <button><AiOutlineMenu/></button>
+            </div>
+        </header>
+    )
+}
